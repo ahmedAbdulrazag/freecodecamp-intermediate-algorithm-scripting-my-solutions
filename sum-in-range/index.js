6c3fa2c4cb549ba85fe71092c9055a3d44874747
Ahmed Altariqi <95758604+ahmedAbdulrazag@users.noreply.github.com>
2022-01-23 17:16:35 +0300

    Find the sum in range

---
+++ sum-in-range/index.js
@@ -0,0 +1,21 @@
+
+const numbers = [4, 1]
+
+const sumAll = arr => {
+    // Find the minimum and maximum numbers so we know 
+    // from where to stat and when to end summing up
+    const min = Math.min(...arr)
+    const max = Math.max(...arr)
+    let sum = 0; 
+
+    // our loop will keep running untill i === max
+    // and in the meanwhile update our sum 
+    for(let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum
+}
+
+const sumInRange = sumAll()
+
+console.log({sumInRange})
